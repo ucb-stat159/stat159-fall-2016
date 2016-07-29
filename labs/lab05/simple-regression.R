c73d970bae0bee5acdae36821c8508155eb1a006
gastonstat <gaston.stat@gmail.com>
2016-07-29 10:08:14 -0700

    labs: add functions confint() to R script in lab 05

--- labs/lab05/simple-regression.R
+++ labs/lab05/simple-regression.R
@@ -3,7 +3,7 @@
 # Title: Simple Linear Regression Analysis
 # Author: Gaston Sanchez
 # Description: 
-#   The main purpose of this script is to carry out the
+#   The main purpose of this script is to carry out a
 #   simple linear regression analysis to obtain the results
 #   displayed in Section 3.1 (chapter 3) of the book
 #   "An Introduction to Statistical Learning"
@@ -16,16 +16,16 @@
 #   http://www-bcf.usc.edu/~gareth/ISL/data.html
 # ===========================================================
 
-# import data
+# read data set in R as a "data.frame"
 # (assuming that the data file is in your working directory)
 dat <- read.csv('Advertising.csv', row.names = 1)
 
-# inspect data frame
-head(dat)
-str(dat)
-summary(dat)
+# inspecting data frame
+head(dat)  # first rows
+str(dat)   # structure
+summary(dat)  # descriptive stats
 
-# because the regression model involves 'TV' and 'Sales'
+# because the simple regression model involves 'TV' and 'Sales'
 # we'll do a bit of exploration of these variables
 hist(dat$TV)
 boxplot(dat$TV)
@@ -40,7 +40,7 @@ cor(dat$TV, dat$Sales)
 
 # simple linear regression with function 'lm()'
 # (i.e. linear model)
-reg = lm(Sales ~ TV, data = dat)
+reg <- lm(Sales ~ TV, data = dat)
 reg
 
 # 'lm()' returns an object of class "lm"
@@ -49,8 +49,18 @@ class(reg)
 # the object 'reg' contains many results
 names(reg)
 
+# likewise, an object of class "lm" has some default plots
+plot(reg)
+
 # an object of class "lm" has a 'summary()' method
-summary(reg)
+regsum <- summary(reg)
+regsum
+
+# the object 'regsum' also contains more results
+names(regsum)
+
+# 95% confidence intervals of regression coefficients
+confint(reg, level = 0.95)
 
 # scatter plot with regression line
 plot(dat$TV, dat$Sales, pch = 19, col = "#8e8f94")
